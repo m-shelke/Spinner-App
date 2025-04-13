0bcfdaed9a7c4849b00c77ad93740ae158629832
AtomicFive4059 <mshelke074@gmail.com>
2025-04-13 12:22:44 +0530

    Spinner with ArrayAdapter

---
+++ app/src/main/java/com/example/spinnerapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.spinnerapp;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    //init XML Views
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        //finding XML Views
+        spinner = findViewById(R.id.spinner);
+
+        //Data source for Spinner
+        String[] courses = {"Java","Kotlin","Python"};
+
+        //ArrayAdapter: used to populate the 'spinner' with items from string array resource
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,courses);
+
+        //apply the Adapter to the XML Spinner
+        spinner.setAdapter(arrayAdapter);
+    }
+}
